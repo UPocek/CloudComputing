e06a3540673a8361d6d6750f994903d843ac071f
Tamara Ilic <tamarailic11@gmail.com>
2023-04-27 11:39:44 +0200

    Background setting

---
+++ front/pages/registration.js
@@ -0,0 +1,7 @@
+import PageContainer from "@/components/page_containter";
+
+export default function RegistrationPage(){
+    return <PageContainer>
+        <h1>Jupii</h1>
+    </PageContainer>
+} 
