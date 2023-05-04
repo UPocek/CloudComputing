e0183006009c4abfea7f7f9ed492bda44cbfc88b
Uros Pocek <uros.pocek@gmail.com>
2023-05-04 20:04:50 +0200

    refactor: Code cleanup

--- cloud/registration/app.py
+++ cloud/registration/app.py
@@ -5,28 +5,30 @@ import random
 client = boto3.resource("dynamodb")
 user_table = client.Table("User")
 
-def registration_lambda(event, context):
 
-    body = event.get('body')
-    
+def registration_lambda(event, context):
+    body = event.get("body")
+
     if body is None:
         return bed_request("Required parameters missing")
-    
+
     body = json.loads(body)
-        
+
     newUser = body.get("newUser")
     if newUser is None:
         return bed_request("Required parameters missing")
-    if(set(newUser.keys()) != set(['name', 'surname', 'birthday', 'username', 'email', 'password'])):
+    if set(newUser.keys()) != set(
+        ["name", "surname", "birthday", "username", "email", "password"]
+    ):
         return bed_request("Required parameters missing")
-    
-    username = newUser['username']
+
+    username = newUser["username"]
 
     user = user_table.get_item(Key={"username": username})
     if user.get("Item") is not None:
         return bed_request("Username already exists!")
 
-    newUser['avatar'] = get_random_avatar(newUser)
+    newUser["avatar"] = get_random_avatar(newUser)
     _ = user_table.put_item(Item=newUser)
     return successfull_registration(newUser)
 
@@ -35,10 +37,10 @@ def bed_request(message):
     return {
         "statusCode": 400,
         "headers": {
-        "Access-Control-Allow-Headers" : "Content-Type",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "POST"
-    },
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "POST",
+        },
         "body": json.dumps({"message": message}),
     }
 
@@ -48,17 +50,17 @@ def successfull_registration(user):
     return {
         "statusCode": 200,
         "headers": {
-        "Access-Control-Allow-Headers" : "Content-Type",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "POST"
-    },
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "POST",
+        },
         "body": json.dumps(user),
     }
 
-def get_random_avatar(user):
-    if (user['name'][-1].lower() == 'a'):
-        letter = 'f'
-    else:
-        letter = random.choice(['m', 'f'])
-    return letter + str(random.randint(0,7))
 
+def get_random_avatar(user):
+    if user["name"][-1].lower() == "a":
+        letter = "f"
+    else:
+        letter = random.choice(["m", "f"])
+    return letter + str(random.randint(0, 7))
--- cloud/upload_file/app.py
+++ cloud/upload_file/app.py
@@ -1,60 +1,76 @@
 import json
 import boto3
 
-dynamodb_client = boto3.resource('dynamodb')
-files_table = dynamodb_client.Table('FilesMetadata')
-s3_client = boto3.client('s3')
-bucket_name = 'cloudcomputingfiles'
+dynamodb_client = boto3.resource("dynamodb")
+files_table = dynamodb_client.Table("FilesMetadata")
+s3_client = boto3.client("s3")
+bucket_name = "cloudcomputingfiles"
+
 
 def upload_file_lambda(event, context):
-        
-    body = event.get('body')
-    
+    body = event.get("body")
+
     if body is None:
         return bed_request("Missing required upload parameters")
-    
+
     body = json.loads(body)
-    
-    fileContent = body.get('fileContent')
-    fileName = body.get('fileName')
-    fileType = body.get('fileType')
-    fileSize = body.get('fileSize')
-    fileCreated = body.get('fileCreated')
-    fileLastModefied = body.get('fileLastModefied')
-    description = body.get('description')
-    tags = body.get('tags')
-    owner = body.get('owner')
-    haveAccsess = body.get('haveAccsess')
-    
+
+    fileContent = body.get("fileContent")
+    fileName = body.get("fileName")
+    fileType = body.get("fileType")
+    fileSize = body.get("fileSize")
+    fileCreated = body.get("fileCreated")
+    fileLastModefied = body.get("fileLastModefied")
+    description = body.get("description")
+    tags = body.get("tags")
+    owner = body.get("owner")
+    haveAccsess = body.get("haveAccsess")
+
     if fileContent is None:
         return bed_request("Missing required upload parameters")
-    
-    files_table.put_item(Item={'fileName':fileName, 'fileType':fileType, 'fileSize':fileSize, 'fileCreated':fileCreated, 'fileLastModefied':fileLastModefied, 'description':description, 'tags':tags, 'owner':owner, 'haveAccsess':haveAccsess})
-    fileBytes = bytes(fileContent, 'UTF-8')
-    s3_client.put_object(Bucket=bucket_name, Key=f'{owner}/{fileName}', Body=fileBytes)
-                
+
+    fileContent = fileContent.split(",")[1]
+
+    print(fileContent)
+
+    files_table.put_item(
+        Item={
+            "fileName": fileName,
+            "fileType": fileType,
+            "fileSize": fileSize,
+            "fileCreated": fileCreated,
+            "fileLastModefied": fileLastModefied,
+            "description": description,
+            "tags": tags,
+            "owner": owner,
+            "haveAccsess": haveAccsess,
+        }
+    )
+    fileBytes = bytes(fileContent, "UTF-8")
+    s3_client.put_object(Bucket=bucket_name, Key=f"{owner}/{fileName}", Body=fileBytes)
+
     return successfull_login(body)
+
 
 def bed_request(message):
     return {
         "statusCode": 400,
         "headers": {
-        "Access-Control-Allow-Headers" : "Content-Type",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "POST"
-    },
-        "body": json.dumps({
-            "message": message
-        }),
-        }
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "POST",
+        },
+        "body": json.dumps({"message": message}),
+    }
+
 
 def successfull_login(file):
     return {
         "statusCode": 200,
         "headers": {
-        "Access-Control-Allow-Headers" : "Content-Type",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "POST"
-    },
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "POST",
+        },
         "body": json.dumps(file),
     }
