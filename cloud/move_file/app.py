2f00b78e8922a15e130cd2f0c6afd23e6b696851
Uros Pocek <uros.pocek@gmail.com>
2023-05-21 00:10:07 +0200

    feat: Move file to another album

--- cloud/move_file/app.py
+++ cloud/move_file/app.py
@@ -1,25 +1,63 @@
 import json
 import boto3
+import base64
 import os
 
 dynamodb_client = boto3.resource("dynamodb")
+files_table = dynamodb_client.Table(os.environ["FILES_TABLE"])
 users_table = dynamodb_client.Table(os.environ["USERS_TABLE"])
 cognito_client = boto3.client("cognito-idp")
+s3_client = boto3.client("s3")
+bucket_name = os.environ["FILES_BUCKET"]
 
 
-def new_album(event, context):
+def move_file(event, context):
     body = json.loads(event["body"])
-    new_album_name = body.get("albumName", "").replace(",", "")
+
+    if body.get("fileContent") is None:
+        return bed_request("Missing required upload parameters")
+
     jwt_token = get_jwt_from_header(event)
     user = get_user_from_cognito(jwt_token)
-
     user = users_table.get_item(Key={"username": user["preferred_username"]})["Item"]
-    if user["albums"].get(new_album_name) is not None:
-        return bed_request("Album with that name already exists")
-    user["albums"][new_album_name] = []
+
+    fileContent = body.get("fileContent")
+    fileName = body.get("fileName")
+    fileType = body.get("fileType")
+    fileSize = body.get("fileSize")
+    fileCreated = body.get("fileCreated")
+    fileLastModefied = body.get("fileLastModefied")
+    description = body.get("description")
+    tags = body.get("tags")
+    owner = body.get("owner")
+    haveAccsess = body.get("haveAccsess")
+
+    user["albums"][os.environ["MAIN_ALBUM_NAME"]].append(f"{owner},{fileName}")
     users_table.put_item(Item=user)
 
-    return successfull_upload(user)
+    fileContent = fileContent.split(",")[1]
+
+    files_table.put_item(
+        Item={
+            "fileName": fileName,
+            "fileType": fileType,
+            "fileSize": fileSize,
+            "fileCreated": fileCreated,
+            "fileLastModefied": fileLastModefied,
+            "description": description,
+            "tags": tags,
+            "owner": owner,
+            "haveAccess": haveAccsess,
+            "albums": ["Main Album"],
+        }
+    )
+    s3_client.put_object(
+        Bucket=bucket_name,
+        Key=f"{owner}/{fileName}",
+        Body=base64.b64decode(fileContent),
+    )
+
+    return successfull_upload(body)
 
 
 def get_jwt_from_header(event):
