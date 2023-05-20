9b26a9b3d57b2d7397fcd23b1e981682eca1aea5
Tamara Ilic <tamarailic11@gmail.com>
2023-05-21 00:11:53 +0200

    feat:Update file

---
+++ cloud/delete_file/app.py
@@ -0,0 +1,81 @@
+import json
+import boto3
+import os
+
+s3_client = boto3.client("s3")
+bucket_name = os.environ["FILES_BUCKET"]
+dynamodb_client = boto3.resource("dynamodb")
+files_table = dynamodb_client.Table(os.environ["FILES_TABLE"])
+users_table = dynamodb_client.Table(os.environ["USERS_TABLE"])
+
+
+def delete_file(event, context):
+    fileName = event["queryStringParameters"].get("fileName")
+    owner = event["queryStringParameters"].get("owner")
+    if fileName is None or owner is None:
+        return bed_request("Required parameters missing")
+
+    file_to_delete = files_table.get_item(
+        Key={"fileName": fileName, "owner": owner}
+    ).get("Item")
+    if file_to_delete is None:
+        return bed_request("File doesn't exist")
+
+    users = file_to_delete["haveAccess"]
+    delete_file_in_users(users, fileName)
+    delete_dynamodb(owner, fileName)
+    is_deleted = delete_s3(owner, fileName)
+
+    return successfull({"fileName": fileName, "deleted": is_deleted})
+
+
+def delete_file_in_users(users, fileName):
+    for username in users:
+        user = users_table.get_item(Key={"username": username}).get("Item")
+        albums = {}
+        for key in user["albums"]:
+            albums[key] = []
+            for userfileName in user["albums"][key]:
+                if userfileName != fileName:
+                    albums[key].append(userfileName)
+        files_table.update_item(
+            Key={"fileName": fileName, "owner": username},
+            UpdateExpression="SET albums = :albums",
+            ExpressionAttributeValues={":albums": albums},
+            ReturnValues="UPDATED_NEW",
+        ).get("Attributes")
+
+
+def delete_dynamodb(owner, fileName):
+    files_table.delete_item(Key={"fileName": fileName, "owner": owner}).get("Item")
+
+
+def delete_s3(owner, fileName):
+    return s3_client.delete_object(
+        Bucket=bucket_name,
+        Key=f"{owner}/{fileName}",
+    ).get("DeleteMarker", False)
+
+
+def bed_request(message):
+    return {
+        "statusCode": 400,
+        "headers": {
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "DELETE",
+        },
+        "body": json.dumps({"message": message}),
+    }
+
+
+def successfull(file):
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "DELETE",
+        },
+        "body": json.dumps(file),
+    }
--- front/pages/index.js
+++ front/pages/index.js
@@ -3,6 +3,7 @@ import { useEffect, useRef, useState } from "react"
 import Image from "next/image";
 import axios from "axios";
 import { baseUrl } from "./_app";
+import { getUserEmail } from "@/helper/helper";
 
 
 export default function Home() {
@@ -87,7 +88,6 @@ function UploadDocumentCard() {
     tagsRef.current.value = '';
     setFileToUpload(null);
     const response = await axios.post(`${baseUrl}/api/upload`, fileData);
-    console.log(response);
   }
 
   function handleDragOver(event) {
@@ -220,7 +220,6 @@ function AlbumDocument({ index, showPreview, doc }) {
 }
 
 function DocumentPreview({ index, setPreview, setAlbum, album }) {
-  const [name, setName] = useState(album[index]['fileName']);
   const [tags, setTags] = useState(album[index]['tags']);
   const [comment, setComment] = useState(album[index]['description']);
 
@@ -228,23 +227,38 @@ function DocumentPreview({ index, setPreview, setAlbum, album }) {
 
   function saveChanges(event) {
     event.preventDefault();
+    const editedFile = {
+      'tags': (album[index]['tags'] != tags ? tags : undefined),
+      'description': (album[index]['description'] != confirm ? comment : undefined),
+    }
 
+    const filteredEditFile = Object.fromEntries(Object.entries(editedFile).filter(([_, v]) => v !== undefined));
+    axios.put(`${baseUrl}/api/update_file`, filteredEditFile, { params: { fileName: album[index]['fileName'], owner: album[index]['owner'] } }).then(_ => setNewValues()).catch(err => console.log(err));
+  }
+
+  function setNewValues() {
     const updatedDocuments = [...album]
-    updatedDocuments[index]['fileName'] = name;
     updatedDocuments[index]['tags'] = tags;
     updatedDocuments[index]['description'] = comment;
-
     setAlbum(updatedDocuments)
     setEditing(false)
   }
 
   function setCurrentValues() {
-    setName(album[index]['fileName']);
     setTags(album[index]['tags']);
     setComment(album[index]['description']);
     setEditing(false)
   }
-  console.log(album)
+
+  function hadleDelete() {
+    axios.delete(`${baseUrl}/api/delete_file`, { params: { fileName: album[index]['fileName'], owner: album[index]['owner'] } }).then(_ => deleteFile()).catch(err => console.log(err));
+  }
+
+  function deleteFile() {
+    const updatedDocuments = [...album].splice(index, 1)
+    setAlbum(updatedDocuments)
+    setEditing(false)
+  }
 
   return <div className={styles.prev_container}>
     <div className={styles.doc_nav}>
@@ -254,14 +268,14 @@ function DocumentPreview({ index, setPreview, setAlbum, album }) {
       <div className={styles.divider}>{album[index]['fileLastModefied']}</div>
       <div className={styles.divider}>
         <div className={styles.icon}>
-          <Image src={'/images/download.png'} width={30} height={30} alt="doc" ></Image>
+          <Image src={'/images/download.png'} width={30} height={30} alt="download" ></Image>
         </div>
-        <div className={styles.icon} onClick={() => setEditing(true)}>
-          <Image src={'/images/edit.png'} width={30} height={30} alt="doc" ></Image>
-        </div>
-        <div className={styles.icon}>
-          <Image src={'/images/delete.png'} width={30} height={30} alt="doc" ></Image>
-        </div>
+        {album[index]['owner'] == getUserEmail() && <div className={styles.icon} onClick={() => setEditing(true)}>
+          <Image src={'/images/edit.png'} width={30} height={30} alt="edit" ></Image>
+        </div>}
+        {album[index]['owner'] == getUserEmail() && <div className={styles.icon} onClick={hadleDelete}>
+          <Image src={'/images/delete.png'} width={30} height={30} alt="delete" ></Image>
+        </div>}
       </div>
 
     </div>
@@ -270,8 +284,8 @@ function DocumentPreview({ index, setPreview, setAlbum, album }) {
         <Image src={'/images/document.png'} width={300} height={300} alt="doc" ></Image>
       </div>
       <div className={styles.doc_details}>
-        <h4>{editing ? <label htmlFor="name">{`Name:`}</label> : 'Name:'}</h4>
-        {editing ? <input type="name" id='name' name="name" value={name} onChange={e => setName(e.target.value)} /> : <p>{album[index]['fileName']}</p>}
+        <h4>'Name:'</h4>
+        <p>{album[index]['fileName']}</p>
 
         <h4>{editing ? <label htmlFor="tags">{`Tags (comma-separated):`}</label> : 'Tags:'}</h4>
         {editing ? <input type="text" id='tags' name="tags" value={tags} onChange={e => setTags(e.target.value)} /> : <p>{album[index]['tags']}</p>}
