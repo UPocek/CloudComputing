b297ce43c351bd651deb671bd2d4dd3e6682b4f5
Uros Pocek <uros.pocek@gmail.com>
2023-05-21 01:25:43 +0200

    fix: Username for delete

--- cloud/delete_album/app.py
+++ cloud/delete_album/app.py
@@ -5,6 +5,9 @@ import os
 dynamodb_client = boto3.resource("dynamodb")
 users_table = dynamodb_client.Table(os.environ["USERS_TABLE"])
 cognito_client = boto3.client("cognito-idp")
+s3_client = boto3.client("s3")
+files_bucket_name = os.environ["FILES_BUCKET"]
+files_table = dynamodb_client.Table(os.environ["FILES_TABLE"])
 
 
 def delete_album(event, context):
@@ -26,6 +29,10 @@ def delete_album(event, context):
 
     for file in user["albums"][album_to_delete]:
         owner, fileName = file.split(",")
+        if owner != user["username"]:
+            delete_someones_file(fileName, owner)
+        else:
+            delete_this_user_file()
 
     del user["albums"][album_to_delete]
     users_table.put_item(Item=user)
@@ -48,6 +55,52 @@ def get_user_from_cognito(jwt):
     for attribute in user["UserAttributes"]:
         current_user[attribute["Name"].replace("custom:", "")] = attribute["Value"]
     return current_user
+
+
+def delete_this_user_file():
+    pass
+
+
+def delete_someones_file(fileName, owner):
+    file_to_delete = files_table.get_item(
+        Key={"fileName": fileName, "owner": owner}
+    ).get("Item")
+    if file_to_delete is None:
+        return bed_request("File doesn't exist")
+
+    users = file_to_delete["haveAccess"]
+    delete_file_in_users(owner, users, fileName)
+    delete_dynamodb(owner, fileName)
+    return delete_s3(owner, fileName)
+
+
+def delete_file_in_users(owner, users, fileName):
+    for username in users:
+        user = users_table.get_item(Key={"username": username}).get("Item")
+        albums = {}
+        for key in user["albums"]:
+            albums[key] = []
+            for userfileName in user["albums"][key]:
+                if userfileName != owner + "," + fileName:
+                    albums[key].append(userfileName)
+
+        users_table.update_item(
+            Key={"username": username},
+            UpdateExpression="SET albums = :albums",
+            ExpressionAttributeValues={":albums": albums},
+            ReturnValues="UPDATED_NEW",
+        ).get("Attributes")
+
+
+def delete_dynamodb(owner, fileName):
+    files_table.delete_item(Key={"fileName": fileName, "owner": owner})
+
+
+def delete_s3(owner, fileName):
+    s3_client.delete_object(
+        Bucket=files_bucket_name,
+        Key=f"{owner}/{fileName}",
+    )
 
 
 def bed_request(message):
--- cloud/delete_file/app.py
+++ cloud/delete_file/app.py
@@ -3,7 +3,7 @@ import boto3
 import os
 
 s3_client = boto3.client("s3")
-bucket_name = os.environ["FILES_BUCKET"]
+files_bucket_name = os.environ["FILES_BUCKET"]
 dynamodb_client = boto3.resource("dynamodb")
 files_table = dynamodb_client.Table(os.environ["FILES_TABLE"])
 users_table = dynamodb_client.Table(os.environ["USERS_TABLE"])
@@ -14,12 +14,12 @@ def delete_file(event, context):
     owner = event["queryStringParameters"].get("owner")
     if fileName is None or owner is None:
         return bed_request("Required parameters missing")
-    is_deleted = delete_file(fileName, owner)
+    is_deleted = delete_one_file(fileName, owner)
 
     return successfull({"fileName": fileName, "deleted": is_deleted})
 
 
-def delete_file(fileName, owner):
+def delete_one_file(fileName, owner):
     file_to_delete = files_table.get_item(
         Key={"fileName": fileName, "owner": owner}
     ).get("Item")
@@ -27,36 +27,36 @@ def delete_file(fileName, owner):
         return bed_request("File doesn't exist")
 
     users = file_to_delete["haveAccess"]
-    delete_file_in_users(users, fileName)
+    delete_file_in_users(owner, users, fileName)
     delete_dynamodb(owner, fileName)
     return delete_s3(owner, fileName)
 
 
-def delete_file_in_users(users, fileName):
+def delete_file_in_users(owner, users, fileName):
     for username in users:
         user = users_table.get_item(Key={"username": username}).get("Item")
         albums = {}
         for key in user["albums"]:
             albums[key] = []
             for userfileName in user["albums"][key]:
-                if userfileName != username + "," + fileName:
+                if userfileName != owner + "," + fileName:
                     albums[key].append(userfileName)
 
-            users_table.update_item(
-                Key={"username": username},
-                UpdateExpression="SET albums = :albums",
-                ExpressionAttributeValues={":albums": albums},
-                ReturnValues="UPDATED_NEW",
-            ).get("Attributes")
+        users_table.update_item(
+            Key={"username": username},
+            UpdateExpression="SET albums = :albums",
+            ExpressionAttributeValues={":albums": albums},
+            ReturnValues="UPDATED_NEW",
+        ).get("Attributes")
 
 
 def delete_dynamodb(owner, fileName):
-    files_table.delete_item(Key={"fileName": fileName, "owner": owner}).get("Item")
+    files_table.delete_item(Key={"fileName": fileName, "owner": owner})
 
 
 def delete_s3(owner, fileName):
     return s3_client.delete_object(
-        Bucket=bucket_name,
+        Bucket=files_bucket_name,
         Key=f"{owner}/{fileName}",
     ).get("DeleteMarker", False)
 
